e372156038eae5f801959428bdbe29491803e5a4
ZecharyDouglas <Zechary.Douglas@cix.csi.cuny.edu>
2023-07-09 22:46:17 -0400

    final?

--- mostWater.test.js
+++
@@ -1,8 +0,0 @@
-const maxArea = require("./mostWater");
-
-test("finds maximum area", () => {
-  expect(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])).toBe(49);
-  expect(maxArea([1, 1])).toBe(1);
-  expect(maxArea([4, 3, 2, 1, 4])).toBe(16);
-  expect(maxArea([1, 2, 1])).toBe(2);
-});
