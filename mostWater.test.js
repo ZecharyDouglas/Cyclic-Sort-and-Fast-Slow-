91c6720e676016cf86a9e6ea6aa8f541e58f5b49
ZecharyDouglas <Zechary.Douglas@cix.csi.cuny.edu>
2023-07-09 21:56:02 -0400

    ++

--- mostWater.test.js
+++ mostWater.test.js
@@ -1,4 +1,4 @@
-const maxArea = require("./mostWater"); // assuming maxArea is exported from maxArea.js
+const maxArea = require("./mostWater");
 
 test("finds maximum area", () => {
   expect(maxArea([1, 8, 6, 2, 5, 4, 8, 3, 7])).toBe(49);
