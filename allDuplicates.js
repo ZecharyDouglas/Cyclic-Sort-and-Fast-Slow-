3ad6a4b2b2feae92ac1461253a3cb92e99d2e60e
ZecharyDouglas <Zechary.Douglas@cix.csi.cuny.edu>
2023-07-09 21:54:53 -0400

    +

--- allDuplicates.js
+++ allDuplicates.js
@@ -1,6 +1,8 @@
 var findDuplicates = function (nums) {
   let i = 0;
   while (i < nums.length) {
+    // implement the cyclic sort and then the elements that
+    // are not at the correct index are the duplicates
     let correct = nums[i] - 1;
     if (nums[i] != nums[correct]) {
       [nums[i], nums[correct]] = [nums[correct], nums[i]];
