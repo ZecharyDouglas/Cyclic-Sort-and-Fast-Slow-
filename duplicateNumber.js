2390a47ebabdf859d501efa53b87be8d11797349
ZecharyDouglas <Zechary.Douglas@cix.csi.cuny.edu>
2023-07-09 21:05:59 -0400

    exports

--- duplicateNumber.js
+++ duplicateNumber.js
@@ -1,7 +1,9 @@
 var findDuplicate = function (nums) {
   let currentIndex = 0;
   while (currentIndex < nums.length) {
+    //compute the correct index for the current element
     const correctIndex = nums[currentIndex] - 1;
+    //
     if (currentIndex !== correctIndex) {
       [nums[currentIndex], nums[correctIndex]] = [
         nums[correctIndex],
@@ -12,3 +14,5 @@ var findDuplicate = function (nums) {
   }
   return nums[nums.length - 1];
 };
+
+module.exports = findDuplicate;
